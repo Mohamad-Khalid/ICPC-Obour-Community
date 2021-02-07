87de64379a8c5d180e541df80708f144cd8339c7
Mohammed Khalid <61889578+Mohamad-Khalid@users.noreply.github.com>
2021-02-07 22:29:07 +0200

    Create B-Calculate the area.cpp

---
+++ Level1 Training/Online Contest #1/B-Calculate the area.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  // first solution
+  // area = width * length
+  // 5 = 1 * 5
+  int n;// area
+  cin >> n;
+  cout << 1 <<" "<<n;
+
+  // second solution 
+  /*int n;
+  cin >> n;
+  for( int i =1;i<=n;i++){
+    for(int j=1; j<=n;j++){
+      if(i*j == n){
+        cout << i <<" "<<j <<endl;
+        return 0;
+      }
+    }
+  }*/
+
+  /*int n;
+  cin >> n;
+  for(int i=1;i<=n;i++){
+    if(n%i==0){
+      cout << i <<" "<<n/i;
+      return 0;
+    }
+  }*/
+ 
+ 
+	return 0;
+} 
