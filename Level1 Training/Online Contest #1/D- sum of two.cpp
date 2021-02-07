3c747c48f8bacb5a59c235ebe6ab6559f14e21b1
Mohammed Khalid <61889578+Mohamad-Khalid@users.noreply.github.com>
2021-02-07 22:33:42 +0200

    Create D- sum of two.cpp

---
+++ Level1 Training/Online Contest #1/D- sum of two.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int t; // number of test cases
+  cin >> t;
+  for(int i = 1 ; i<= t ;i++){ 
+
+    int a,b;
+    cin >> a>>b;
+    cout << a+b << endl;
+
+  }
+
+	return 0;
+} 
